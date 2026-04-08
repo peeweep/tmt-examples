6c8a4c891d80bb980303ee5e01ed3dec3ef6ed5f
jinqiang zhang <peeweep@0x0.ee>
2026-04-08 12:54:23 +0800

    change package2/tests/example2 to beakerlib, this is a broken testcase

--- SPECS/package2/tests/example2/test.sh
+++ SPECS/package2/tests/example2/test.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-cat /etc/os-release | grep openruyi
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlRun "tmp=\$(mktemp -d)" 0 "Create tmp directory"
+        rlRun "pushd $tmp"
+        rlRun "set -o pipefail"
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        rlRun "cat /etc/os-release | tee output" 0 "Check os-release"
+        rlAssertGrep "fedora" "output"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "popd"
+        rlRun "rm -r $tmp" 0 "Remove tmp directory"
+    rlPhaseEnd
+rlJournalEnd
